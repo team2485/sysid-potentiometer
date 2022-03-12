50fa1434c79bd31442f20803ac25ef2fee8daeae
Mark <mrifkin2022@francisparker.org>
2022-03-11 18:21:38 -0800

    edits and cleanup

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -36,32 +36,23 @@ frc::AnalogPotentiometer analogPot{0, 2*M_PI, -M_PI};
 
 std::vector<double> m_data;
 
-std::string m_testType; 
+std::string m_testType = "Quasistatic";
+std::string m_direction = "Forward";
 
-double m_motorVoltage = 0.0; 
+double m_motorVoltage; 
 
 double m_lastPosition = 0.0;
 
 double stepVoltage = 3.0;
 
-double rampRate = 0.2; 
-
-// tmpfile;
-
- //std::string sysidLogs = "C:/home/lvuser/sysidLogs/sysid.txt";
-
+double rampRate = 0.1; 
 
 Robot::Robot() : frc::TimedRobot(5_ms){
-  try {
-    //m_json = GetConfigJson();
-
-  } catch (std::exception& e) {
-    fmt::print("FAILED: \n", e.what());
-    std::exit(-1);
-  }
 }
 
-void Robot::RobotInit() {}
+void Robot::RobotInit() {
+
+}
 
 void Robot::RobotPeriodic() {}
 
@@ -73,36 +64,28 @@ void Robot::AutonomousInit() {
 
 void Robot::AutonomousPeriodic() {
   m_lastPosition = analogPot.Get();
-  std::string type = m_testType == "Dynamic" ? "fast" : "slow";
-  m_motorVoltage = m_talonsrx.GetMotorOutputVoltage();
-  std::string direction = m_motorVoltage > 0 ? "forward" : "backward";
-  if (m_testType == "fast") {
-    if (direction == "forward") {
-      m_motorVoltage = stepVoltage;
-      m_talonsrx.SetVoltage((units::volt_t) m_motorVoltage);
+  
+  if (m_testType == "Dynamic") {
+    if (m_direction == "Forward") {
+      m_talonsrx.SetVoltage((units::volt_t) stepVoltage);
     }
     else {
-      m_motorVoltage = -stepVoltage;
-      m_talonsrx.SetVoltage((units::volt_t) m_motorVoltage);
+      m_talonsrx.SetVoltage((units::volt_t) -stepVoltage);
     }
   }
   else {
-    if(direction == "forward") {
-      m_motorVoltage = rampRate * (frc::Timer::GetFPGATimestamp().value() - m_data.front());
-      m_talonsrx.SetVoltage((units::volt_t) m_motorVoltage);
+    if(m_direction == "Forward") {
+      m_talonsrx.SetVoltage((units::volt_t)  rampRate * (frc::Timer::GetFPGATimestamp().value() - m_data.front()));
     }
     else {
-      m_motorVoltage = - rampRate * (frc::Timer::GetFPGATimestamp().value() - m_data.front());
-      m_talonsrx.SetVoltage((units::volt_t) m_motorVoltage);
+      m_talonsrx.SetVoltage((units::volt_t) - rampRate * (frc::Timer::GetFPGATimestamp().value() - m_data.front()));
     }
   }
 
   double m_position = analogPot.Get();
-  //const double m_timeStepInit = frc::Timer::Get();
   double m_potVelocity = (m_position - m_lastPosition) / 0.005; 
   
-  //m_data.insert( m_data.begin(), {type, direction});
-  m_data.insert( m_data.end(), {m_talonsrx.GetMotorOutputVoltage(), m_position, m_potVelocity});
+    m_data.insert( m_data.end(), {m_talonsrx.GetMotorOutputVoltage(), m_position, m_potVelocity});
 
   m_lastPosition = analogPot.Get();
 
@@ -114,46 +97,11 @@ void Robot::TeleopPeriodic() {}
 
 void Robot::DisabledInit() {
   m_motorVoltage = 0.0; 
-  //frc::SmartDashboard::PutBoolean("SysIdOverflow", m_data.size() >= 3600);
-  //string to json
-  //     std::stringstream ss;
-  //     for (int i = 0; i < m_data.size(); ++i) {
-  //       ss << std::to_string(m_data[i]);
-  //       if (i < m_data.size() - 1) {
-  //         ss << ",";
-  //   } 
-  // }
-    //could use test below to prefix test name OR leave it as first term in vector (current V does both)
+  
     std::string type = m_testType == "Dynamic" ? "fast" : "slow";
     std::string direction = m_motorVoltage > 0 ? "forward" : "backward";
     std::string test = fmt::format("{}-{}", type, direction);
-    //FILE WRITING ATTEMPT LOLZ
-    // fs::ofstream file;
-    // fs::file.open("C:/home/lvuser/sysidLogs/sysid.txt");
-    // //o << 
 
-    // if (!file.is_open()) {
-    //   fmt::print("FAILED");
-    // }
-    // else {
-    //   file.write(m_data.data(), m_data.size());
-    //   fmt::print("SUCCEEDED");
-    // }
-
-    // file.close();
-    // const fs::path& path = "/home/lvuser/sysidLogs/sysid.json";
-    // fs::create_directories(path().root_directory());
-
-    // std::error_code ec;
-    // wpi::raw_fd_ostream ostream{path.string(), ec};
-
-    // if (ec) {
-    //   throw std::runtime_error("FAILED: " + ec.message());
-    // }
-
-    // ostream << ss;
-
-    
     ofstream MyFile;
     string filename("/home/lvuser/sysidLogs/sysid.json");
 
